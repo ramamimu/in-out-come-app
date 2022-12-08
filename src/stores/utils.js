2e2f7ff7b1df4f029934cf7e9046ee1546d216c1
Arga Dewanata <rerearga@gmail.com>
2022-12-08 21:55:57 +0700

    Add Utils Configuration

---
+++ src/stores/utils.js
@@ -0,0 +1,31 @@
+export const MUTATION = {
+  income: 0,
+  outcome: 1,
+};
+
+export const PAYMENT_METHOD = {
+  bankTransfer: 0,
+  cash: 1,
+  dana: 2,
+  shopeePay: 3,
+  gopay: 4,
+  ovo: 5,
+};
+
+export const INCOME_CATEGORY = {
+  salary: 0,
+  sideHustle: 1,
+  other: 2,
+};
+
+export const OUTCOME_CATEGORY = {
+  foodAndDrink: 0,
+  rent: 1,
+  transportation: 2,
+  subscription: 3,
+  internet: 4,
+  grocery: 5,
+  entertainment: 6,
+  clothing: 7,
+  furniture: 8,
+};
