4113a3a281a88a45994f9f5901144875d3830cce
ramamimu <ahhmaad.blogger@gmail.com>
2023-05-12 18:05:12 +0700

    feat: cloudbuild

--- src/stores/socket.js
+++ src/stores/socket.js
@@ -12,23 +12,23 @@ export const useSocketIO = defineStore("socketIO  ", {
       // this.socket = io(`http://192.168.236.24:${port_web_socket}/chart`);
       // this.socket = io(`localhost:${port_web_socket}/chart`);
       // this.socket = io(`${window.location.host}/chart`);
-      const THAT = this;
+      // const THAT = this;
       this.socket = io(
         `http://${window.location.hostname}:${port_web_socket}/chart`
       );
-      this.interval = setInterval(() => {
-        console.log(this.socket);
-        if (!this.socket.connected) {
-          THAT.socket = io(
-            `http://${window.location.hostname}:${port_web_socket}/chart`
-          );
-          console.log("try to reconnect to server");
-        } else {
-          clearInterval(THAT.interval);
-          console.log("clear the interval");
-        }
-        console.log(this.socket);
-      }, 5000);
+      // this.interval = setInterval(() => {
+      //   console.log(this.socket);
+      //   if (!this.socket.connected) {
+      //     THAT.socket = io(
+      //       `http://${window.location.hostname}:${port_web_socket}/chart`
+      //     );
+      //     console.log("try to reconnect to server");
+      //   } else {
+      //     clearInterval(THAT.interval);
+      //     console.log("clear the interval");
+      //   }
+      //   console.log(this.socket);
+      // }, 5000);
     },
     emitUIToServer(emitter, data) {
       const THAT = this;
